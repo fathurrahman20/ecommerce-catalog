3173b698939b3c0a9389326ce0fa0dc79dc00c9f
Fathurrahman <10190031@bsi.ac.id>
2025-06-16 00:23:40 +0700

    feat: add home page

---
+++ src/components/Welcome.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="landing-container">
+    <div class="info-card">
+      <h1>Welcome to Catalog.</h1>
+      <p>
+        This project was developed as part of the Project Based Internship
+        program by Core Initiative in collaboration with Rakamin Academy.
+      </p>
+
+      <p class="features-title">Key Features Include:</p>
+
+      <ul class="features-list">
+        <li>Dynamic data fetching from the live Fake Store API.</li>
+        <li>Interactive navigation to cycle through products.</li>
+        <li>A modern, component-based structure using Vue.js.</li>
+      </ul>
+
+      <div class="button-wrapper">
+        <RouterLink to="/products/1" class="cta-button"
+          >Explore Products</RouterLink
+        >
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Welcome from "@/components/Welcome.vue";
+</script>
+
+<template>
+  <main class="container">
+    <Welcome />
+  </main>
+</template>
+
+<style>
+.container {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  font-family: "Poppins", sans-serif;
+  background-color: #eeeeee;
+  color: #333;
+}
+</style>
